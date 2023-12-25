713c244f9fbb71171f6662399280b989901ac8bc
b-cakes <115378509+b-cakes@users.noreply.github.com>
2023-12-25 15:51:34 -0600

    adjusted tokyo-cat colorscheme

--- nvim/lua/custom/autocommands.lua
+++ nvim/lua/custom/autocommands.lua
@@ -19,6 +19,7 @@ vim.api.nvim_create_autocmd("FileType", {
 	end
 })
 
+-- creating dark background windows for Telescope when using the randomhue cmd from mini.hues
 vim.api.nvim_create_autocmd("ColorScheme", {
 	pattern = "randomhue",
 	command = "hi link TelescopeNormal NormalFloat | hi link TelescopePromptNormal NormalFloat",
