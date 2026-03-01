bdf5f51b628fad71bde7ac08c9f9874009f96c9d
b-cakes <115378509+b-cakes@users.noreply.github.com>
2026-03-01 08:52:47 -0600

    tring again 3

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -24,11 +24,7 @@ config.underline_position = -2
 config.enable_kitty_graphics = true
 config.cursor_blink_rate = 390
 
-<<<<<<< Updated upstream
 config.color_scheme = 'rose-pine-moon'
-=======
-config.color_scheme = 'tokyo-cat'
->>>>>>> Stashed changes
 
 
 -- Use 'ColorschemeDump wezterm' command to paste the current neovim colorscheme into the below format. 
